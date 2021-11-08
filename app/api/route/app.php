b4b069ecdd4276f54dec8bdc3651b7d2cdabf60e
杨小伟 <2389469307@qq.com>
2021-11-08 22:58:35 +0800

    update api跨域 上传图片

--- app/api/route/app.php
+++ app/api/route/app.php
@@ -11,4 +11,5 @@
 use think\facade\Route;
 
 Route::get('login', 'Login/getUsers')->middleware(\app\middleware\Check::class);
+Route::get('upload', 'Upload/index');
 Route::get('swipers', 'Swiper/getSwipers')->middleware(\app\middleware\Check::class);
